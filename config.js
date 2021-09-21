fbf7571418a95421beb9c46f474b3041af9fd122
Wahyu OFC <90292909+wahyuofc@users.noreply.github.com>
2021-09-21 16:20:59 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,52 @@
+global.owner = ['6282325549376']
+global.autoread = true
+global.selfmode = false
+global.mess = {
+group : {
+welcome : `Welcome @user\n\nSelamat datang di grup @subject`,
+bye : `Good Bye @user`,
+promote: '@user Sekarang admin!',
+demote: '@user Sekarang bukan admin!'
+},
+error : 'Terjadi Kesalahan',
+success: 'Sukses...'
+}
+global.server = false
+global.prefix = '🐤'
+global.author = '@caliph91_'
+global.packname = 'WhatsApp Bot'
+
+
+// LIST APIKEY
+
+global.APIs = { // API Prefix
+
+  clph: 'https://recoders-area.caliph.repl.co',
+  rikka: 'https://rikka-api.herokuapp.com',
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  lol: 'https://api.lolhuman.xyz',
+  vh: 'http://api.vhtear.com',
+  zeks: 'https://api.zeks.me',
+  caliphAPI: 'https://api-caliph.herokuapp.com'
+}
+
+global.APIKeys = { // APIKey Here
+   'http://api.vhtear.com': 'YOUR-APIKEY',
+   'https://api.lolhuman.xyz': 'YOUR-APIKEY',
+  'https://api.xteam.xyz': 'YOUR-APIKEY'
+,
+  'https://api.zeks.me': 'rikkabotwa',
+  'https://rikka-api.herokuapp.com': 'beta',
+   'https://api-caliph.herokuapp.com': 'beta'
+}
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
